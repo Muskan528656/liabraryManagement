618fc1ae2464b905e54b8cacc3e3edd603d1b1ec
Muskan-009 <muskankhan83908@gmail.com>
2025-11-21 15:42:11 +0530

    Code

--- frontend/src/components/common/userdatamanager.js
+++ frontend/src/components/common/userdatamanager.js
@@ -6,10 +6,7 @@ export const useDataManager = (dependencies = {}, propsData = {}) => {
   const [data, setData] = useState({});
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
-///hgfgh
-//hgfgahjsdhgfgahsjdhgshjdhgshdjjshgdhj
 
-//sdfghjkjhgfdfghj
   useEffect(() => {
     const fetchAllData = async () => {
       try {
