bbdddd1444ba69f9567f5041d0280a3e2115376e
Muskan Khan <muskan.k@ibirdsservices.com>
2026-01-06 11:46:08 +0530

    Abid sir code

--- frontend/src/components/bookissue/BulkIssue.js
+++ frontend/src/components/bookissue/BulkIssue.js
@@ -191,32 +191,43 @@ const BulkIssue = () => {
       return;
     }
 
+    // const filtered = books.filter(book => {
+    //   const min = book.min_age !== null && book.min_age !== "" ? Number(book.min_age) : null;
+    //   const max = book.max_age !== null && book.max_age !== "" ? Number(book.max_age) : null;
+
+    //   //check also zero age books 
+    //   if (min === 0 && max === 0) {
+    //     return true;
+    //   }
+
+    //   //if member age is zero  
+    //   if (memberAge === 0) {
+    //     return min === null && max === null;
+    //   }
+
+    //   if (min === null) {
+    //     return false;
+    //   }
+    //   if (min < memberAge) {
+    //     return false;
+    //   }
+    //   if (max !== null && memberAge > max) return false;
+
+    //   return true;
+    // });
+
     const filtered = books.filter(book => {
       const min = book.min_age !== null && book.min_age !== "" ? Number(book.min_age) : null;
       const max = book.max_age !== null && book.max_age !== "" ? Number(book.max_age) : null;
 
-      //check also zero age books 
-      if (min === 0 && max === 0) {
-        return true;
-      }
 
-      //if member age is zero  
-      if (memberAge === 0) {
-        return min === null && max === null;
-      }
+      if (min === null || max === null) return false;
+      if (min === 0 && max === 0) return false;
 
-      if (min === null) {
-        return false;
-      }
-      if (min < memberAge) {
-        return false;
-      }
-      if (max !== null && memberAge > max) return false;
+      if (memberAge === null || memberAge === undefined) return false;
 
-      return true;
+      return max <= memberAge;
     });
-
-
 
     console.log("Member Age:", memberAge);
     console.table(filtered.map(b => ({
