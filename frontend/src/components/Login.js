2fe5d540502646b90c715c8c6d6e3f90d8312aa1
Muskan Khan <muskan.k@ibirdsservices.com>
2026-02-05 13:00:55 +0530

    Code chnages

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -264,7 +264,7 @@ const Login = () => {
                   minHeight: "100vh",
                 }}>
                   {/* <img src="Untitled design (7).png" width="600" height="600" alt="Logo" /> */}
-                  <img src="Ibirds.png" width="200" height="200" alt="Logo" />
+                  <img src="ibirds.png" width="600" height="600" alt="Logo" />
                 </Col>
 
                 <Col lg={6} className="p-5">
